59d25adaa65ddd70af4a4583521decf675570d01
João Pedro V. Pansiere <ojp027@gmail.com>
2024-11-26 08:36:18 -0300

    update

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static selectRaw(string $string)
+ * @method static create(array|int[]|null[]|string[] $array_merge)
+ * @method static where(string $string, string $id)
+ */
 class Transaction extends Model
 {
     protected $table = 'transactions';
